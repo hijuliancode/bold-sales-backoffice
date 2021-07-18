188f38b6b35e6d305f681cbf59bd954338abe241
hijuliancode <hijuliansosa@gmail.com>
2021-07-18 00:51:29 -0500

    Bold | Home : Layout, Screen and assets

---
+++ src/modules/home/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import React from 'react'
+
+export const HomeScreen = () => {
+  return (
+    <div className="page page-home">
+      <Head>
+        <title>Bold - Vende más con Bold, el datáfono que recibe todas la tarjetas</title>
+        <meta name="description" content="Imagina poder tener un datáfono en tu negocio en tan solo minutos, sin procesos, o limitantes. Una vez lo compres es tuyo; no tendrás que pagar mensualidad." />
+        <meta name="keywords" content="datáfono, adquirir datáfono, comprar datáfono, vender más, tarjeta ,débito, tarjeta crédito, recibir pagos, aumentar ventas, redeban, credibanco, minidatafono, datáfono colombia, pagos con tarjeta, venta datáfono" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>HOME</h1>
+      </main>
+    </div>
+  )
+}
